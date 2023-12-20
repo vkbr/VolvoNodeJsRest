2be4f2495c96f8b9bcb58ad1206a4f15e612b2b8
Vivek Brohma <vivekb@Viveks-MBP.lan>
2023-12-20 22:53:23 +0100

    removing bin

--- bin/middlewares/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.middlewares = void 0;
-const auth_1 = require("./auth");
-exports.middlewares = [...auth_1.authMiddlewares];
